32a185cf46f7e561878a18164906dc0b04f43dec
ericzzh <zhangzhehuan@gmail.com>
2026-06-25 19:30:03 +0800

    some keymap + opencode.nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,9 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map('t', '<Esc>', [[<C-\><C-n>]], { silent = true })
+map('v', '<', '<gv')
+map('v', '>', '>gv')
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- manually added
